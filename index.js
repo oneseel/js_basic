905449d123b93e0670f745142c6c5c26461f76b5
oneseel <seno10065@gmail.com>
2024-11-19 00:21:25 +0900

    파라미터 실습

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function welcome(name) {
-  console.log("안녕하세요 " + name + "님");
+// 여기에 코드를 작성하세요
+function teraToGiga(volume) {
+  console.log(volume + "TB는");
+  console.log(volume * 1024 + "GB 입니다");
 }
 
-welcome("javascript");
-welcome("react");
-welcome("nodejs");
-
-function printSquare(x) {
-  console.log(x * x);
+function teraToMega(volume) {
+  console.log(volume + "TB는");
+  console.log(volume * 1024 * 1024 + "MB 입니다");
 }
 
-printSquare(2);
-printSquare(5);
-printSquare(6);
+// TB -> GB 테스트
+teraToGiga(2);
+// TB -> MB 테스트
+teraToMega(2);

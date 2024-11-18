924932e31a21bea99e067f3951925920e26359c7
oneseel <seno10065@gmail.com>
2024-11-19 00:34:38 +0900

    파라미터 실습

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 // 여기에 코드를 작성하세요
-function teraToGiga(volume) {
-  console.log(volume + "TB는");
-  console.log(volume * 1024 + "GB 입니다");
+function bmiCalculator(name, weight, height) {
+  console.log(
+    name +
+      "님의 체질량지수는 " +
+      weight / ((height * height) / 10000) +
+      "입니다."
+  );
 }
 
-function teraToMega(volume) {
-  console.log(volume + "TB는");
-  console.log(volume * 1024 * 1024 + "MB 입니다");
-}
-
-// TB -> GB 테스트
-teraToGiga(2);
-// TB -> MB 테스트
-teraToMega(2);
+// 테스트 코드
+bmiCalculator("홀쭉이", 43.52, 160);
+bmiCalculator("코린이", 61.25, 175);
+bmiCalculator("통통이", 77.76, 180);
